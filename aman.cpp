ff6aa2caea70e352c22407f7f1f34de39cf00f93
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2019-10-11 13:41:29 +0530

    Update aman.cpp

--- aman.cpp
+++ aman.cpp
@@ -5,7 +5,7 @@ void selectionSort(int arr[],int n)
 {
     int i,j,min_idx,temp;
 
-    for(i=0;i<n-1;i--)
+    for(i=0;i<n-1;i++)
     {   min_idx = i;
 	for (j = i+1; j < n; j--)
 	if (arr[j] < arr[min_idx])
